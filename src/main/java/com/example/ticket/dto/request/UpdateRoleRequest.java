0a732e23015ed8ef204231be454ce0679c90262b
NguyenTuan180505 <hung0935347609@gmail.com>
2026-06-29 14:34:44 +0700

    feat: add AdminController and AdminUserService

---
+++ src/main/java/com/example/ticket/dto/request/UpdateRoleRequest.java
@@ -0,0 +1,14 @@
+package com.example.ticket.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateRoleRequest {
+    @NotBlank
+    private String roleName;
+}
